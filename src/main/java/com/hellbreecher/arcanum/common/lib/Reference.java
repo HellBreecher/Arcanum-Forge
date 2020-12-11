694554d9a46377d22a857d1d1855d264e183d364
HellBreecher <HellBreecher@users.noreply.github.com>
2020-12-10 23:35:24 -0600

    Update to 6.0.0.0-beta2

--- src/main/java/com/hellbreecher/arcanum/common/lib/Reference.java
+++ src/main/java/com/hellbreecher/arcanum/common/lib/Reference.java
@@ -3,7 +3,7 @@ package com.hellbreecher.arcanum.common.lib;
 public class Reference {
     public static final String MODID = "arcanum";
     public static final String NAME = "Arcanum";
-    public static final String VERSION = "1.16.4-6.0.0.0-beta1";
+    public static final String VERSION = "1.16.4-6.0.0.0-beta2";
     public static final String Client_Proxy = "com.hellbreecher.arcanum.client.ClientProxy";
     public static final String Server_Proxy = "com.hellbreecher.arcanum.common.CommonProxy";
     public static final String updateJson = "https://raw.githubusercontent.com/HellBreecher/Arcanum/master/update.json";
