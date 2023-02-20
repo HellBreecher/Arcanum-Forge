b436732fcddbc8cfdf68e47bd050f8fd3340fcc7
HellBreecher <HellBreecher@users.noreply.github.com>
2023-02-19 20:22:01 -0600

    Update ArcanumTags.java

--- src/main/java/com/hellbreecher/arcanum/common/util/ArcanumTags.java
+++ src/main/java/com/hellbreecher/arcanum/common/util/ArcanumTags.java
@@ -10,18 +10,19 @@ import net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.Tags;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.registries.RegistryObject;
 
 @Mod.EventBusSubscriber(modid = Reference.MODID, bus = EventBusSubscriber.Bus.MOD)
 public class ArcanumTags {
 	
-	public static class Blocks {
+	public static class RegisterBlockTags {
 		
 		public static final TagKey<Block> NEEDS_GREENSAPPHIRE_TOOL = create("needs_greensapphire_tool");
 		public static final TagKey<Block> NEEDS_BLOODDIAMOND_TOOL = create("needs_blooddiamond_tool");
 		public static final TagKey<Block> NEEDS_VOIDDIAMOND_TOOL = create("needs_voiddiamond_tool");
 		public static final TagKey<Block> NEEDS_INFERNALDIAMOND_TOOL = create("needs_infernaldiamond_tool");
 		public static final TagKey<Block> NEEDS_INFERNAL_TOOL = create("needs_infernal_tool");
-
+		
 		
 		private static TagKey<Block> create(String name) {
 			return BlockTags.create(new ResourceLocation(Reference.MODID, name));
@@ -41,12 +42,12 @@ public class ArcanumTags {
     {
         return switch(tier)
                 {
-                    case GreenSapphireTool -> ArcanumTags.Blocks.NEEDS_GREENSAPPHIRE_TOOL;
+                    case GreenSapphireTool -> ArcanumTags.RegisterBlockTags.NEEDS_GREENSAPPHIRE_TOOL;
                     case BloodDiamondTool -> Tags.Blocks.NEEDS_GOLD_TOOL;
                     case VoidDiamondTool -> BlockTags.NEEDS_STONE_TOOL;
                     case InfernalDiamondTool -> BlockTags.NEEDS_IRON_TOOL;
                     case InfernalTool -> BlockTags.NEEDS_DIAMOND_TOOL;
-                    default -> ArcanumTags.Blocks.NEEDS_GREENSAPPHIRE_TOOL;
+                    default -> ArcanumTags.RegisterBlockTags.NEEDS_GREENSAPPHIRE_TOOL;
                 };
     }
 }
